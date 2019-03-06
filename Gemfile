a04fa00d69681ded7ab728df0b2d65345ae47c79
tomoakimiura <tomomiura0026@gmail.com>
2019-03-06 10:38:19 +0900

    basic picturetweet

--- Gemfile
+++ Gemfile
@@ -63,3 +63,8 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'pry-rails'
 gem 'compass-rails','3.1.0'
 gem 'sprockets', '3.7.2'
+gem 'kaminari'
+gem 'devise'
+gem 'cancancan'
+gem 'rails_admin'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
-  Rails.application.routes.draw do
-    get 'tweets' => 'tweets#index'
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  root  'tweets#index'
+  resources :tweets do
+    resources :comments, only: [:create]
   end
+  resources :users, only: [:show]
+end
