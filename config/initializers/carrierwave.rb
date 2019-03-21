c0c031a8e9f79686daf2b33e8e3c8ef78672e778
tomoakimiura <tomomiura0026@gmail.com>
2019-03-21 21:52:01 +0900

    weather.js実装前

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'tomoakipacket'
+  config.fog_directory  = 'picturetweet-upload'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/picturetweet-upload'
 end
